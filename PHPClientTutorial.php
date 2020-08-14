5df0887006ab128599dec9113e202c6b36122efe
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-08-14 13:17:07 +0300

    Update to latest version

--- PHPClientTutorial.php
+++ PHPClientTutorial.php
@@ -1,5 +1,5 @@
 <?php
-/** (1) Set variables such as "host" and "port" **/
+/** (1) Set variables such as "host" and "port" and "message" **/
 $host = "127.0.0.1";
 $port = 5353;
 $message = " Hello server ! 123";
@@ -15,7 +15,7 @@ $result = socket_connect($socket, $host, $port) or die("Could not connect toserv
 socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
 
 /** (5)  Read the response from the server**/
-$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+$result = socket_read($socket, 1024) or die("Could not read server response\n");
 echo "Reply From Server  :".$result;
 
 /** (6) Close the socket */

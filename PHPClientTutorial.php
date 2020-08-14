2a558a69180667351ed27b112417a78d04188788
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-08-14 13:10:20 +0300

    Add intial project files

---
+++ PHPClientTutorial.php
@@ -0,0 +1,23 @@
+<?php
+/** (1) Set variables such as "host" and "port" **/
+$host = "127.0.0.1";
+$port = 5353;
+$message = " Hello server ! 123";
+// No Timeout 
+set_time_limit(0);
+/** (2) Create Socket **/
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+
+/** (3) Connect to the server **/
+$result = socket_connect($socket, $host, $port) or die("Could not connect toserver\n");
+
+/** (4) Write to server socket **/
+socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+
+/** (5)  Read the response from the server**/
+$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+echo "Reply From Server  :".$result;
+
+/** (6) Close the socket */
+socket_close($socket);
+?>
